bf27a8e03f8fd39e22cdbf18475c17c7ebe8cd6a
Javantax <79591148+Javantax@users.noreply.github.com>
2024-09-24 15:52:29 -0300

    Tutorial stuff

--- CS1083/Practice/Polymorphism-2/NoiseDriver.java
+++ CS1083/Practice/Polymorphism-2/NoiseDriver.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 public class NoiseDriver {
     public static void main(String[] args) {
         HouseCat cat = new HouseCat("Alfred", 10, 5, 17, 20, "Connor");
         Phone phone = new Phone("Denny");
         Coyote coyote = new Coyote("Bruce", 10, 5, 12, 40);
+        Fox fox = new Fox("Frank", 10, 5, 13, 14);
 
+        ArrayList<Audible> objects = new ArrayList<>();
+        objects.add(cat);
+        objects.add(phone);
+        objects.add(coyote);
+        objects.add(fox);
+
+        for (Audible a : objects) {
+            System.out.println(a.makeNoise());
+        }
     }
 }
