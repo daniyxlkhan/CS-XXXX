c45f14a1696ae658d1d662938a2e18f2aeee4661
Javantax <79591148+Javantax@users.noreply.github.com>
2025-05-14 14:09:28 -0300

    Lab1 for CS2263 2

--- CS2263/Labs/Lab1/Exercise4.c
+++ CS2263/Labs/Lab1/Exercise4.c
@@ -9,12 +9,16 @@ int main() {
 }
 
 void printFib(int n) {
-    int a = 0, b = 0, temp;
+    int a = 0, b = 1, temp;
     if (n <= 0) {
         return;
     }
+    
     printf("%d ", a);
     for (int i = 1; i < n; i++) {
         printf("%d ", b);
+        temp = a + b;
+        a = b;
+        b = temp;
     }
 }
